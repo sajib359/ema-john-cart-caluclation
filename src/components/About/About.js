cdbc343dde0fce10141e48224378fbc643862cec
Sajib Hossain <sajib.sj359@gmail.com>
2022-03-31 12:51:20 +0600

    Added all Routes

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>Hi This Is Sajib</h1>
+            <p>I am trying to create E-commerce site!!!</p>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/components/Orders/Orders.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Orders = () => {
+    return (
+        <div>
+            <h1>This Is Oder Page!!!</h1>
+        </div>
+    );
+};
+
+export default Orders;
